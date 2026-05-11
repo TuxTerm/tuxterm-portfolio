958baacb8cf3fe5aa9db14651fdc70d907416261
tuxterm <rishuraj0175@gmail.com>
2026-05-11 11:23:31 +0530

    Remove phone number from contact section

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -3,7 +3,6 @@
   <p>You can reach me via email or find me on various platforms:</p>
   <ul>
     <li><a href="mailto:rishuraj0175@gmail.com">Email (rishuraj0175@gmail.com)</a></li>
-    <li><a href="tel:+919470445872">Phone (+91-9470445872)</a></li>
     <li><a href="https://github.com/TuxTerm" target="_blank" rel="noopener noreferrer">GitHub (@TuxTerm)</a></li>
     <li><a href="https://linkedin.com/in/rishuraj0175" target="_blank" rel="noopener noreferrer">LinkedIn (/in/rishuraj0175)</a></li>
   </ul>
