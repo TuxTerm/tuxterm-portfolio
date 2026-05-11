00f4aa17bd04b8d481af8675e15deff693c0f7e9
tuxterm <rishuraj0175@gmail.com>
2026-05-11 10:18:34 +0530

    Update About section with new bio

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,11 +1,19 @@
 <section id="about">
   <h1><span class="highlight">^_^ Hello World.</span></h1>
-  <p>I am <strong>Rishu Raj</strong>, an aspiring Cybersecurity Professional.</p>
   <p>
-    Currently studying Information Security, networking, and system administration. 
-    Passionate about ethical hacking, defensive security, and learning how systems break to better protect them.
+    Hey, there! I'm <strong>Rishu Raj</strong>. I am an organized mess of some random molecules existing to form something called 'life' in human form. This life was born in Haryana, India and this life is currently 17 years old (though according to science, almost all my cells have been replaced by new cells, so am I me even?).
   </p>
-  <p>Status: Actively seeking internships and entry-level roles.</p>
+
+  <h3>The Tech Side</h3>
+  <p>
+    The neurons in my brain are very crazy about logical stuff.<br/>
+    I do and turn on computers and linux. I like programming and testing out random stuff on Linux.
+  </p>
+
+  <h3>My System</h3>
+  <p>
+    I use Linux on my system. A detailed description of applications and tools I use is available at <a href="#skills">Skills</a>.
+  </p>
   
   <br/>
   <h3>Certifications</h3>
@@ -16,7 +24,7 @@
   <h3>Achievements</h3>
   <ul>
     <li>Secured AIR 41st on NCIIPC-AICTE Pentathon 2025 FINALE.</li>
-    <li>Lead my team for SIH.</li>
+    <li>Attended NCIIPC-AICTE Pentathon 2026 Bootcamp at MIT Manipal and interacted with various industry leaders. </li>
   </ul>
 </section>
 
